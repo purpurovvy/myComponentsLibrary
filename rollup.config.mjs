20195fcb69c6762164028aef5cac4b9edf5960fa
purpurovvy <purpurovvy@gmail.com>
2024-01-23 11:36:58 +0100

    changed configuration

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,36 +1,37 @@
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 import terser from '@rollup/plugin-terser';
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
-export default {
-    input: 'src/index.ts',
-    output: [
-        {
-            file: 'dist/index.js',  // Another output file for the bundle
-            format: 'esm',  // The format of the second output bundle
-            sourcemap: false,
-            inlineDynamicImports: true,
-        },
-        {
-            file: 'dist/index.cjs.js',  // The file to output the bundle to
-            format: 'cjs',  // The format of the output bundle
-            sourcemap: false,  // Generate source map
-            inlineDynamicImports: true,  // Inline dynamic imports in the generated bundle
-        },
-    ],
-    plugins: [
-        typescript({
-            tsconfig: './tsconfig.json',  // Path to the TypeScript configuration file
-        }),
-        resolve(),
-        commonjs(),
-        terser()
-    ],
-    onwarn(warning, warn) {
-        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
-            return
-        }
-        warn(warning)
-    }
-};
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: 'dist/index.cjs.js',
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: 'dist/index.js',
+                format: "esm",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            typescript({tsconfig: "./tsconfig.json"}),
+            terser(),
+        ],
+        external: ["react", "react-dom", "@emotion/react", "@emotion/styled", "@mui/icons-material", "@mui/material"],
+    },
+    {
+        input: "src/index.ts",
+        output: [{file: "dist/types.d.ts", format: "es"}],
+        plugins: [dts()],
+    },
+];
